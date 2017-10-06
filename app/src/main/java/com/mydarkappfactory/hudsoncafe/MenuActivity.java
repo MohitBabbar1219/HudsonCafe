cff6cedded65878e0860a66890c3015d00058efc
Mohit <mohitbabbar1219@gmail.com>
2017-10-06 19:43:53 +0530

    Navigation drawer added

--- app/src/main/java/com/mydarkappfactory/hudsoncafe/MenuActivity.java
+++ app/src/main/java/com/mydarkappfactory/hudsoncafe/MenuActivity.java
@@ -1,13 +1,62 @@
 package com.mydarkappfactory.hudsoncafe;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.navigation_drawer);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MenuActivity.this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.itemOne:
+                //TODO
+                break;
+            case R.id.itemTwo:
+                //TODO
+                break;
+            case R.id.itemThree:
+                //TODO
+                break;
+            case R.id.cart:
+                //TODO
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
