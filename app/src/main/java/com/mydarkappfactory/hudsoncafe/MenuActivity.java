a341bfa4ee7b26b88d333aaea3610ec822119bfb
Mohit <mohitbabbar1219@gmail.com>
2017-10-08 11:23:29 +0530

    Menu ViewPager added

--- app/src/main/java/com/mydarkappfactory/hudsoncafe/MenuActivity.java
+++ app/src/main/java/com/mydarkappfactory/hudsoncafe/MenuActivity.java
@@ -3,6 +3,7 @@ package com.mydarkappfactory.hudsoncafe;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,18 +11,32 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class MenuActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    ViewPager viewPager;
+    SlideshowAdapter adapter;
+    CircleIndicator indicator;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_drawer);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        indicator = (CircleIndicator) findViewById(R.id.circleIndicator);
+        adapter = new SlideshowAdapter(MenuActivity.this);
+
+        viewPager.setAdapter(adapter);
+        indicator.setViewPager(viewPager);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
---
+++ app/src/main/java/com/mydarkappfactory/hudsoncafe/SlideshowAdapter.java
@@ -0,0 +1,67 @@
+package com.mydarkappfactory.hudsoncafe;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+public class SlideshowAdapter extends PagerAdapter{
+
+    private Context context;
+
+    LayoutInflater inflater;
+
+    public int[] images = {
+            R.drawable.appetizers,
+            R.drawable.main_course,
+            R.drawable.shakes
+    };
+
+    public SlideshowAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view == (LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slideshow_layout, container, false);
+        ImageView img = (ImageView) view.findViewById(R.id.slideshowImage);
+//        img.setImageResource(images[position]);
+
+        Glide.with(context).load(images[position]).into(img);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((LinearLayout) object);
+
+    }
+}
